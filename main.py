b72d958c3db34e30f2beb38498e2a04e9742db18
diraison <jean.diraison@ac-rennes.fr>
2021-05-09 19:57:41 +0200

    suppression d'un module inutile et amélioration du code de main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+"""
+Ensemble de macros-fonctions outils servant à intégrer les différents
+composants d'un exercice pyexo dans une page web (titre, énoncé,
+indication...). Il repose sur le module mkdocs-macros-plugin.
+"""
+
 import base64
 import zlib
-import urllib.parse
+
 
 def define_env(env):
     "Hook function"
-    
+
     @env.macro
     def pyexo(chemin, baseurl="", width="100%", height=480):
         with open(chemin) as fichier:
@@ -12,7 +18,7 @@ def define_env(env):
             exo_comp = zlib.compress(exercice.encode())
             exo_cb64 = base64.urlsafe_b64encode(exo_comp).decode()
             return f"""<iframe src="{baseurl}?clear=titre1:titre2:enonce&exo={exo_cb64}" width={width} height={height}></iframe>"""
-    
+
     @env.macro
     def pyexo_parser(chemin):
         titre1, titre2, enonce, solution, tests, code, indication, unitaires = ["INDEFINI"] * 8
@@ -30,22 +36,22 @@ def define_env(env):
             unitaires, _, reste = reste.partition(separateur)
         return {"titre1":titre1, "titre2":titre2, "enonce":enonce, "solution":solution,
                 "tests":tests, "code":code, "indication":indication, "unitaires":unitaires}
-    
+
     @env.macro
     def pyexo_titre(chemin):
         infos = pyexo_parser(chemin)
-        titre1 = infos["titre1"].replace("\n"," ").replace("\r"," ").strip()
-        titre2 = infos["titre2"].replace("\n"," ").replace("\r"," ").strip()
-        return f"""{titre1} \- {titre2}"""
-    
+        titre1 = infos["titre1"].replace("\n", " ").replace("\r", " ").strip()
+        titre2 = infos["titre2"].replace("\n", " ").replace("\r", " ").strip()
+        return f"""{titre1} \\- {titre2}"""
+
     @env.macro
     def preparer_markdown(chaine):
         if "<pre>" in chaine:
-            chaine = chaine.replace("<pre>","<pre><code>").replace("</pre>","</code></pre>")
+            chaine = chaine.replace("<pre>", "<pre><code>").replace("</pre>", "</code></pre>")
         else:
-            chaine = chaine.replace("\n"," ").replace("\r"," ")
+            chaine = chaine.replace("\n", " ").replace("\r", " ")
             for tag in "*_":
-                chaine = chaine.replace(tag,"\\"+tag)
+                chaine = chaine.replace(tag, "\\"+tag)
         return chaine.strip()
 
     @env.macro
@@ -53,11 +59,10 @@ def define_env(env):
         infos = pyexo_parser(chemin)
         enonce = preparer_markdown(infos["enonce"])
         return f"""{enonce}"""
-    
+
     @env.macro
     def pyexo_indication(chemin):
         infos = pyexo_parser(chemin)
         indication = preparer_markdown(infos["indication"])
         indication = indication or "Pas d'indication pour cet exercice."
         return f"""{indication}"""
-
